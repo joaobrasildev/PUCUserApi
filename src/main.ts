873cd50a42c7d0b7da897c3cc1c31f1ec4a94a70
Joao Brasil <jvobsystems@gmail.com>
2022-05-10 19:41:05 -0300

    finishing config to deploy

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,17 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import env from './config/env';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: ['*'],
+      allowedHeaders: [
+        'Content-Type',
+        'Authorization',
+      ],
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      credentials: true,
+    },
+  });
 
   const config = new DocumentBuilder()
     .setTitle('User API')
@@ -13,7 +23,8 @@ async function bootstrap() {
     .build();
 
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('/docs', app, document);
+  SwaggerModule.setup('docs', app, document);
+  app.enableCors({credentials: true, origin: "*"});
   
   await app.listen(env().application.port);
 }
--- src/shared/dtos/getOne.dto.ts
+++ src/shared/dtos/getOne.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsDate, IsDateString, IsNotEmpty, IsNumber, IsOptional, IsString } from "class-validator";
+import { IsNotEmpty, IsString } from "class-validator";
 
 export class GetOneDTO {
   @ApiProperty()
